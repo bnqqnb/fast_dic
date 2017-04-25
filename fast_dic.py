b5e43f5fbc742b1db8c546dc9cbdcb1bbae5d66d
bnqqnb <bnqqnb@hotmail.com>
2017-04-25 21:31:02 +0800

    modify copy UTF-8 word

--- fast_dic.py
+++ fast_dic.py
@@ -1,16 +1,18 @@
 #! python3
 #fast_dic.py - Search Dictionary from clipboard
-import requests,bs4,sys,pyperclip,webbrowser
+#version 0.0.1
+import requests,bs4,sys,webbrowser,tkinter,urllib
+from tkinter import Tk, Label
+
+word = ''
 if len(sys.argv) > 1:
     word = ' '.join(sys.argv[1:])
 else:
-    word = pyperclip.paste()
-    
-#type(res);
-#res.raise_for_status()
-#noStarchSoup = bs4.BeautifulSoup(res.text)
-#type(noStarchSoup)
+    #unable to use pyperclip,clipboard to copy utf-8 character from clipboard in mac so use Tk()
+    temp = Tk()
+    word = temp.clipboard_get()
+    temp.destroy()    
+url_word = urllib.parse.quote_plus(word) 
+webbrowser.open('http://www.linguee.com/german-english/search?source=auto&query=' + url_word)
 
-webbrowser.open('http://www.linguee.com/german-english/search?source=auto&query=' + word)
 
-print(word);
